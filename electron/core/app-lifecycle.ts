f34bf94566044da091a123f0890223428f041fa5
alexg-sh <me@alexg.sh>
2025-11-07 02:46:26 +0000

    Fix import statements for HelperServiceManager

--- electron/core/app-lifecycle.ts
+++ electron/core/app-lifecycle.ts
@@ -13,7 +13,7 @@ import {
 import { cleanupOrphanedDatabases, cleanupDatabaseTempFiles } from "../utils/cleanup-utils"
 import { autoStartDatabases } from "../database/auto-start"
 import { createWindow, registerAppProtocolHandler } from "../window/window-manager"
-import HelperServiceManager from "../helper-service"
+import { HelperServiceManager } from "../helper-service"
 import PermissionsManager from "../permissions"
 import { IDatabase } from "../../types/database"
 
