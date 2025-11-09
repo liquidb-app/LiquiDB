71797ed340d14ac313e5f396562d2eca68209b91
alexg-sh <me@alexg.sh>
2025-11-09 00:02:03 +0000

    chore: enable signed release commits in semantic-release

--- release.config.js
+++ release.config.js
@@ -95,7 +95,8 @@ module.exports = {
       '@semantic-release/git',
       {
         assets: ['package.json', 'CHANGELOG.md'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        signed: true
       }
     ],
     [
