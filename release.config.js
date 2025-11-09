4902068a7a997df63c6dd25300d1cc20850ecbd8
alexg-sh <me@alexg.sh>
2025-11-09 01:18:16 +0000

    fix: more release fixes

--- release.config.js
+++ release.config.js
@@ -107,7 +107,8 @@ module.exports = {
       {
         draft: true,
         successComment: false,
-        releasedLabels: false
+        releasedLabels: false,
+        assets: false
       }
     ]
   ]
