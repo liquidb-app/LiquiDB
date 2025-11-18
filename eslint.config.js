09640594681b93bed7ddb8b2b2b695552ef88695
alexg-sh <me@alexg.sh>
2025-11-18 12:37:59 +0000

    chore: ignore ric.sh script and cleanup eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -13,23 +13,23 @@ try {
   tsParser = require("@typescript-eslint/parser");
   tsPlugin = require("@typescript-eslint/eslint-plugin");
 } catch (e) {
-  // Parser/plugin not available
+
 }
 try {
   reactHooksPlugin = require("eslint-plugin-react-hooks");
 } catch (e) {
-  // Plugin not available
+
 }
 
-// Load Next.js configs - handle circular reference issues
+
 let nextConfigs = [];
 let hasNextConfigs = false;
 try {
   nextConfigs = compat.extends("next/core-web-vitals", "next/typescript");
   hasNextConfigs = true;
 } catch (error) {
-  // If loading fails, we'll skip Next.js-specific rules
-  // This is expected due to circular reference issues with FlatCompat
+
+
   nextConfigs = [];
 }
 
